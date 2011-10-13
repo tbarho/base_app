5d2ef54e64118eec703bc1f925006ed30e1f615f
Ty Barho <t.barho@gmail.com>
2011-10-13 21:49:38 +0000

    User Signup done, spec passes

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,5 @@
 Factory.define :user do |f|
-  f.sequence(:name) { |n| "test#{n}" }
-  f.sequence(:name) { |n| "test#{n}@example.com" }
+  f.sequence(:username) { |n| "test#{n}" }
+  f.sequence(:email) { |n| "test#{n}@example.com" }
   f.password "secret"
 end
--- spec/requests/user_signups_spec.rb
+++ spec/requests/user_signups_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "UserSignups" do
   before(:each) do
     visit signup_path
-    fill_in "user_name", :with => "tester"
+    fill_in "user_username", :with => "tester"
     fill_in "user_email", :with => "test@example.com"
     fill_in "user_password", :with => "secret"
     fill_in "user_password_confirmation", :with => "secret"
@@ -16,15 +16,15 @@ describe "UserSignups" do
   end
 
   it "should validate name, email and password appropriately" do
-    fill_in "user_name", :with => ""
+    fill_in "user_username", :with => ""
     fill_in "user_email", :with => ""
     fill_in "user_password", :with => ""
     fill_in "user_password_confirmation", :with => ""
     click_button "Sign Up"
-    current_path.should eq(signup_path)
+    current_path.should eq(users_path)
     page.should have_content("Password digest can't be blank")
-    page.should have_content("Name can't be blank")
-    page.should have_content("Name is too short (minimum is 5 characters)")
+    page.should have_content("Username can't be blank")
+    page.should have_content("Username is too short (minimum is 5 characters)")
     page.should have_content("Email can't be blank")
     page.should have_content("Email is invalid")
     page.should have_content("Password can't be blank")
@@ -33,15 +33,14 @@ describe "UserSignups" do
 
   it "should not allow duplicate names or emails" do
     user = Factory(:user)
-    fill_in "user_name", :with => user.name
+    fill_in "user_username", :with => user.username
     fill_in "user_email", :with => user.email
     fill_in "user_password", :with => "secret"
     fill_in "user_password_confirmation", :with => "secret"
     click_button "Sign Up"
-    current_path.should eq(signup_path)
-    page.should have_content("Name already exists")
-    page.should have_content("Email already exists")
-    
+    current_path.should eq(users_path)
+    page.should have_content("Username has already been taken")
+    page.should have_content("Email has already been taken")
   end
 
 
